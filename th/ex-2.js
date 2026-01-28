acbec4db5ae20239bc86fab65f6ac71867c7236c
Phi <ptp.thamboonrak@gmail.com>
2026-01-28 10:59:38 +0700

    feat: implement moveZeroes function and add romanMap for romanToInt conversion

--- th/ex-2.js
+++ th/ex-2.js
@@ -1,5 +1,26 @@
+const romanMap = {
+  I:1,
+  V:5,
+  X:10,
+  L:50,
+  C:100,
+  D:500,
+  M:1000,
+};
+
 const romanToInt = function (s) {
-  //Start coding here
+  let sum = 0;
+for (let i = 0; i < s.length; i++){
+  if (romanMap[s[i]] < (romanMap[s[i+1]])){
+    let dif = romanMap[s[i+1]] - romanMap[s[i]];
+    sum+= dif;
+    i++;
+  }
+  else{
+    sum += romanMap[s[i]];
+  }
+}
+return sum
 };
 
 const result1 = romanToInt("III"); // 3
